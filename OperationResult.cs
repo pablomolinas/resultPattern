0cc9aab915a9db23e32342dfb9fe7ffe113add79
Pablo Molinas <pablo.molinas@yahoo.com>
2021-12-02 11:09:47 -0300

    success generic version

--- OperationResult.cs
+++ OperationResult.cs
@@ -5,52 +5,70 @@ using System;
 namespace resultPattern
 {
     // 
-    public class OperationResult
+    public class Result
     {
         public bool Success { get; protected set; }
         public string FailureMessage { get; protected set; }
         public Exception Exception { get; protected set; }
-        
+                
         // Constructor para caso de exito
-        protected OperationResult()
+        protected Result()
         {
             this.Success = true;
         }
 
         // Constructor para situacion de fallo
-        protected OperationResult(string message)
+        protected Result(string message)
         {
             this.Success = false;
             this.FailureMessage = message;
         }
 
         // Constructor para Exception
-        protected OperationResult(Exception ex)
+        protected Result(Exception ex)
         {
             this.Success = false;
             this.Exception = ex;
         }
         
         // Metodo estatico para resultado de exito
-        public static OperationResult SuccessResult()
+        public static Result SuccessResult()
         {
-            return new OperationResult();
+            return new Result();
         }
 
         // Metodo estatico para situacion de fallo con mensaje
-        public static OperationResult FailureResult(string message)
+        public static Result FailureResult(string message)
         {
-            return new OperationResult(message);
+            return new Result(message);
         }
 
         // Metodo estatico para fallo con exception
-        public static OperationResult ExceptionResult(Exception ex)
+        public static Result ExceptionResult(Exception ex)
         {
-            return new OperationResult(ex);
+            return new Result(ex);
         }
         public bool IsException()
         {
             return this.Exception != null;
+        }
+    }
+
+    // Hereda de Result, siendo generico me permite incluir cualquier tipo de dato en SuccessResult
+    public class Result<T> : Result
+    {
+        public T Payload { get; protected set; }
+
+        protected Result(T t)
+        {
+            this.Success = true;
+            this.Payload = t;
+        }
+
+        // Metodo estatico para resultado de exito
+        public static Result SuccessResult(T t)
+        {
+            return new Result<T>(t);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,14 +6,16 @@ namespace resultPattern
     {
         static void Main(string[] args)
         {
-            var success = OperationResult.SuccessResult();
-            var failWithMessage = OperationResult.FailureResult("fallo inesperado");
-            var excep = OperationResult.ExceptionResult(new Exception("falla interceptada"));
+            var success = Result.SuccessResult();
+            var failWithMessage = Result.FailureResult("fallo inesperado");
+            var excep = Result.ExceptionResult(new Exception("falla interceptada"));
+
+            var successData = Result<string>.SuccessResult("Resultado exitoso");
 
             Console.WriteLine($"Success value: {success.Success}, isExeption: {success.IsException()}");
             Console.WriteLine($"Fail > success: {failWithMessage.Success}, failmessage: {failWithMessage.FailureMessage}, isExeption: {success.IsException()}");
-            Console.WriteLine($"Fail > success: {excep.Success}, failmessage: {excep.FailureMessage}, except: {excep.Exception}, isExeption: {success.IsException()}");
-
+            Console.WriteLine($"Fail > success: {excep.Success}, failmessage: {excep.FailureMessage}, except: {excep.Exception}, isExeption: {excep.IsException()}");
+            Console.WriteLine($"Fail > success: {successData.Success} failmessage: {successData.FailureMessage}, except: {successData.Exception}, isExeption: {successData.IsException()}, payload: {successData}");
 
         }
     }
